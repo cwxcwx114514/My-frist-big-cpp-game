24a995b7bc61e1660284fe20693da915099ce692
CaoWenxiang <2754054494@qq.com>
2026-05-31 14:57:15 +0800

    完成了Imgui相关的设置,现在它可以使用了

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include<iostream>
+
 #include"raylib.h"
+#include"imgui.h"
+#include"rlimgui.h"
 
 int main()
 {
@@ -7,16 +11,43 @@ int main()
 
 	InitWindow(Width, Hight, "myGame");
 
+	SetTargetFPS(60);
+	rlImGuiSetup(true);
+	
+
 	while (!WindowShouldClose())
 	{
-		
-
+		ClearBackground(RAYWHITE);
 		BeginDrawing();
-			ClearBackground(RAYWHITE);
-			DrawText("Hello!!", 200, 300, 40, PINK);
-		EndDrawing();
+		rlImGuiBegin();
+			
+		ImGui::Begin("window_1");
+		if (ImGui::Button("Button##1"))
+		{
+			std::cout << "hhhhhh\n";
+		}
+		ImGui::SameLine();
+		if(ImGui::Button("Button##2"))
+		{
+			std::cout << "hhhhhh\n";
+		}
 
+		ImGui::Text("HHH");
+		//ImGui::NewLine();
+		ImGui::Separator();
+		ImGui::Text("This is a new line.");
+		ImGui::End();
+
+		ImGui::Begin("Window_2");
+		static float a = 0.0f;
+		ImGui::SliderFloat("speed", &a, 0, 1);
+		ImGui::End();
+
+		rlImGuiEnd();
+		EndDrawing();
 	}
+
+	rlImGuiShutdown();
 	CloseWindow();
 	return 0;
 }
